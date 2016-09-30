154f2382a14533dc4bcc5661721a5f83e4cca573
Manoj Fernando <manojf@99x.lk>
2016-09-30 11:46:10 +0530

    Adding more starwars names

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ var uniqueRandomArray = require('unique-random-array');
 var starWarsCharacters = require('./starwars-chracters');
 
 module.exports = {
-    all: starWarsCharacters,
-    random: uniqueRandomArray(starWarsCharacters)
+    getAll: starWarsCharacters,
+    randomName: uniqueRandomArray(starWarsCharacters)
 };
